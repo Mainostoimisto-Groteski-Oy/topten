ea56354f008aa90efff509ea6d5110f2996795fe
unknown <petri@groteski.fi>
2023-03-20 15:07:08 +0200

    PDFn tallennustesti

--- blocks/card-blocks/card-title.php
+++ blocks/card-blocks/card-title.php
@@ -4,5 +4,5 @@ $title_tag = get_field( 'title_tag' ) ?: 'h2';
 $title = get_field( 'title' );
 
 if ( $title ) :
-	echo sprintf( '<%1$s class="card-title">%2$s</%1$s>', esc_attr( $title_tag ), wp_kses_post( get_field( 'title' ) ) );
+	echo sprintf( '<%1$s class="title">%2$s</%1$s>', esc_attr( $title_tag ), wp_kses_post( get_field( 'title' ) ) );
 endif;
