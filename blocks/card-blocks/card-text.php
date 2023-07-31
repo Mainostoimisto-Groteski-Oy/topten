afad35476aee44458a3b8201d70f7b81ed36625a
Ville Laitinen <ville@groteski.fi>
2023-07-31 12:36:18 +0300

    wasdfgds

--- blocks/card-blocks/card-text.php
+++ blocks/card-blocks/card-text.php
@@ -15,72 +15,104 @@ if ( $tulkinta && 'none' !== $tulkinta['value'] ) :
 <div class="card-text-block column" style="<?php topten_get_block_width(); ?>">
 	<?php topten_get_desc(); ?>
 
-	<?php
-	echo wp_kses(
-		$text,
-		array(
-			'a'      => array(
-				'href'   => array(),
-				'title'  => array(),
-				'style'  => array(),
-				'target' => array(),
-			),
-			'b'      => array(
-				'style' => array(),
-			),
-			'br'     => array(
-				'style' => array(),
-			),
-			'em'     => array(
-				'style' => array(),
-			),
-			'h1'     => array(
-				'style' => array(),
-			),
-			'h2'     => array(
-				'style' => array(),
-			),
-			'h3'     => array(
-				'style' => array(),
-			),
-			'h4'     => array(
-				'style' => array(),
-			),
-			'h5'     => array(
-				'style' => array(),
-			),
-			'h6'     => array(
-				'style' => array(),
-			),
-			'i'      => array(
-				'style' => array(),
-			),
-			'img'    => array(
-				'alt' => array(),
-				'src' => array(),
-			),
-			'li'     => array(
-				'style' => array(),
-			),
-			'ol'     => array(
-				'style' => array(),
-			),
-			'p'      => array(
-				'style' => array(),
-			),
-			'strong' => array(
-				'style' => array(),
-			),
-			'ul'     => array(
-				'style' => array(),
-			),
-			'sup'    => array(
-				'style' => array(),
-			),
-			'sub'    => array(
-				'style' => array(),
-			),
-		)
-	);
-	?>
+	<div class="text-wrapper <?php echo $color ? esc_html( 'bg-' . $color ) : ''; ?> <?php echo $tulkinta ? esc_html( $tulkinta['value'] ) : ''; ?>">
+		<?php if ( $tulkinta && 'none' !== $tulkinta['value'] ) : ?>
+			<?php
+			switch ( $tulkinta['value'] ) :
+				case 'law':
+					$icon = '§';
+					break;
+				case 'info':
+					$icon = 'i';
+					break;
+				case 'person':
+					$icon = 'engineering';
+					break;
+				case 'check':
+					$icon = 'done';
+					break;
+				default:
+					$icon = '';
+			endswitch;
+			?>
+			<div class="tulkinta">
+				<p class="<?php echo esc_attr( trim( $class ) ); ?>">
+					<span class="material-symbols tulkinta-symbol" aria-hidden="true">
+						<?php echo esc_html( $icon ); ?>
+					</span>
+
+					<?php echo esc_html( $tulkinta['label'] ); ?>
+				</p>
+			</div>
+		<?php endif; ?>
+
+		<?php
+		echo wp_kses(
+			$text,
+			array(
+				'a'      => array(
+					'href'   => array(),
+					'title'  => array(),
+					'style'  => array(),
+					'target' => array(),
+				),
+				'b'      => array(
+					'style' => array(),
+				),
+				'br'     => array(
+					'style' => array(),
+				),
+				'em'     => array(
+					'style' => array(),
+				),
+				'h1'     => array(
+					'style' => array(),
+				),
+				'h2'     => array(
+					'style' => array(),
+				),
+				'h3'     => array(
+					'style' => array(),
+				),
+				'h4'     => array(
+					'style' => array(),
+				),
+				'h5'     => array(
+					'style' => array(),
+				),
+				'h6'     => array(
+					'style' => array(),
+				),
+				'i'      => array(
+					'style' => array(),
+				),
+				'img'    => array(
+					'alt' => array(),
+					'src' => array(),
+				),
+				'li'     => array(
+					'style' => array(),
+				),
+				'ol'     => array(
+					'style' => array(),
+				),
+				'p'      => array(
+					'style' => array(),
+				),
+				'strong' => array(
+					'style' => array(),
+				),
+				'ul'     => array(
+					'style' => array(),
+				),
+				'sup'    => array(
+					'style' => array(),
+				),
+				'sub'    => array(
+					'style' => array(),
+				),
+			)
+		);
+		?>
+	</div>
 </div>
