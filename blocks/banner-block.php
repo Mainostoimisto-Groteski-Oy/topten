7912e12fa46264fc486d327576dbd5f395cf7e4d
Ville Laitinen <ville@groteski.fi>
2024-01-25 16:49:13 +0200

    wrote gro_gro_the_field in last commit

--- blocks/banner-block.php
+++ blocks/banner-block.php
@@ -1,4 +1,4 @@
-<section class="banner-block <?php gro_gro_the_field( 'color' ); ?>">
+<section class="banner-block <?php gro_the_field( 'color' ); ?>">
 	<div class="grid">
 		<div class="content
 		<?php
@@ -9,7 +9,7 @@
 			<?php topten_block_title(); ?>
 			<?php if ( get_field( 'text' ) ) : ?>
 				<div class="text">
-					<?php gro_gro_the_field( 'text' ); ?>
+					<?php gro_the_field( 'text' ); ?>
 				</div>
 			<?php endif; ?>
 		</div>
